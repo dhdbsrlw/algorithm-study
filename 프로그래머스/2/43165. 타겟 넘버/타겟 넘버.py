e3de08fa32cd014357bf04948e4ecd06cf28db5d
오윤진 Yoonjin Oh <dhdbsrlw@korea.ac.kr>
2023-11-24 14:04:59 +0900

    [level 2] Title: 타겟 넘버, Time: 4.42 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,17 @@
+# DFS 풀이
+def solution(numbers, target):
+    answer = DFS(numbers, target, 0) 
+    return answer
+
+def DFS(numbers, target, depth):
+    answer = 0
+    if depth == len(numbers):
+        # print(numbers)
+        if sum(numbers) == target:
+            return 1
+        else: return 0
+    else:
+        answer += DFS(numbers, target, depth+1) # 재귀 이용
+        numbers[depth] *= -1
+        answer += DFS(numbers, target, depth+1) # 재귀 이용
+        return answer
